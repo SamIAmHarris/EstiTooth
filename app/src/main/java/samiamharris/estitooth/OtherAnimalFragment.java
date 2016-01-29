aecd3b936380f118aa36c8afb9a449643b016e44
Sam Harris <samh@jackrabbitmobile.com>
2016-01-29 16:55:42 -0600

    Add multiple animals to animal swiping view pager. Adjust play button to be in center.

---
+++ app/src/main/java/samiamharris/estitooth/OtherAnimalFragment.java
@@ -0,0 +1,29 @@
+package samiamharris.estitooth;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by SamMyxer on 1/29/16.
+ */
+public class OtherAnimalFragment extends Fragment {
+
+    TextView animalTitleTextView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_other_animal, container, false);
+
+        animalTitleTextView = (TextView) rootView.findViewById(R.id.fragment_swipe_animal_title);
+
+        return rootView;
+    }
+
+
+}
---
+++ app/src/main/java/samiamharris/estitooth/RoosterFragment.java
@@ -0,0 +1,36 @@
+package samiamharris.estitooth;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by SamMyxer on 12/8/15.
+ */
+public class RoosterFragment extends Fragment {
+
+    TextView animalTitleTextView;
+
+    private String[] animales = {"Elephant", "Tiger", "Rooster", "Hippo", "Jeff Zhao"};
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_rooster, container, false);
+
+        animalTitleTextView = (TextView) rootView.findViewById(R.id.fragment_swipe_animal_title);
+
+        return rootView;
+    }
+
+
+    public void setAnimalTitle(int position) {
+        animalTitleTextView.setText(animales[position]);
+    }
+
+}
