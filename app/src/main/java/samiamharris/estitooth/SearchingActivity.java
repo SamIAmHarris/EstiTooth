c6cf28970936009b7fe30d02e049ab4177e8ed23
Sam Harris <samh@jackrabbitmobile.com>
2016-01-29 16:26:17 -0600

    Go to rooster after 5 seconds of launch on search intent for Demo

--- app/src/main/java/samiamharris/estitooth/SearchingActivity.java
+++ app/src/main/java/samiamharris/estitooth/SearchingActivity.java
@@ -1,39 +1,21 @@
 package samiamharris.estitooth;
 
-import android.app.Notification;
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Slide;
 import android.transition.Transition;
 import android.view.Gravity;
 
-import com.estimote.sdk.Beacon;
-import com.estimote.sdk.BeaconManager;
-import com.estimote.sdk.Region;
-import com.estimote.sdk.Utils;
-
-import java.util.List;
-import java.util.UUID;
 
 /**
  * Created by SamMyxer on 1/28/16.
  */
 public class SearchingActivity extends AppCompatActivity {
 
-    BeaconManager beaconManager;
-    private Region region;
-
-    private Beacon currentBeacon;
-
-    public static final int MINT_BEACON = 53140;
-    public static final int BLUEBERRY_BEACON = 47233;
-    public static final int ICE_BEACON = 36878;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,60 +23,17 @@ public class SearchingActivity extends AppCompatActivity {
         setEnterExitTransitions();
         setContentView(R.layout.activity_searching);
 
-        beaconManager = new BeaconManager(this);
-        region = new Region("ranged region",
-                UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+        Handler handler = new Handler();
 
-        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
-            @Override
-            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
-                respondToBeaconDiscovered(list);
+
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                sendRoosterIntent();
             }
-        });
+        }, 5000);
+
     }
 
-    public void respondToBeaconDiscovered(List<Beacon> list) {
-        if (!list.isEmpty()) {
-            Beacon nearestBeacon = list.get(0);
-            Utils.Proximity proximity = Utils.computeProximity(nearestBeacon);
-            if(currentBeacon == null || nearestBeacon.getMajor() != currentBeacon.getMajor()) {
-                if(proximity == Utils.Proximity.IMMEDIATE || proximity == Utils.Proximity.NEAR) {
-                    currentBeacon = nearestBeacon;
-                    switch (nearestBeacon.getMajor()) {
-                        case BLUEBERRY_BEACON:
-                            sendBakingIntent();
-                            break;
-                        case MINT_BEACON:
-                            sendRoosterIntent();
-                            break;
-                        case ICE_BEACON:
-                            break;
-                    }
-                }
-            }
-        } else {
-            if(currentBeacon != null) {
-                currentBeacon = null;
-            }
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
-            @Override
-            public void onServiceReady() {
-                beaconManager.startRanging(region);
-            }
-        });
-    }
-
-    @Override
-    protected void onPause() {
-        beaconManager.stopRanging(region);
-        super.onPause();
-    }
 
     public void setEnterExitTransitions() {
         Transition left = new Slide(Gravity.LEFT);
@@ -119,40 +58,5 @@ public class SearchingActivity extends AppCompatActivity {
         ActivityCompat.startActivity(SearchingActivity.this, roosterIntent, options.toBundle());
     }
 
-    public void sendBakingIntent() {
-        Intent bakingIntent = new Intent(getBaseContext(), EnteredBakingActivity.class);
 
-        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(
-                // the context of the activity
-                SearchingActivity.this, null
-        );
-        ActivityCompat.startActivity(SearchingActivity.this, bakingIntent, options.toBundle());
-    }
-
-    public void sendNotification(String eventDescription, String title, String content) {
-        int notificationId = 001;
-        Intent searchIntent = new Intent(this, SearchingActivity.class);
-        PendingIntent mainPendingIntent =
-                PendingIntent.getActivity(this, 0, searchIntent, 0);
-
-        NotificationCompat.BigTextStyle bigStyle = new NotificationCompat.BigTextStyle();
-        bigStyle.bigText(eventDescription);
-
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setDefaults(Notification.DEFAULT_ALL)
-                        .setContentTitle(title)
-                        .setContentText(eventDescription)
-                        .setContentIntent(mainPendingIntent)
-                        .setStyle(bigStyle);
-
-        // Get an instance of the NotificationManager service
-        NotificationManagerCompat notificationManager =
-                NotificationManagerCompat.from(this);
-
-        // Build the notification and issues it with notification manager.
-        notificationManager.notify(notificationId, notificationBuilder.build());
-
-    }
 }
